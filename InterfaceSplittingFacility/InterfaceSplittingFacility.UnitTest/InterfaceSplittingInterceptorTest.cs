d3c5e9101ced76828cb90456dd9218f1bd9cee38
Lukáš Lánský <lukas@lansky.name>
2015-11-29 12:57:48 -0800

    + RhinoMocks, StyleCop analyzer

---
+++ InterfaceSplittingFacility/InterfaceSplittingFacility.UnitTest/InterfaceSplittingInterceptorTest.cs
@@ -0,0 +1,20 @@
+﻿namespace InterfaceSplittingFacility.UnitTest
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class InterfaceSplittingInterceptorTest
+    {
+        [TestMethod]
+        public void InterceptorCallsSimpleMethodMatch()
+        {
+
+        }
+
+        [TestMethod]
+        public void InterceptorPassSimpleMethodMismatch()
+        {
+
+        }
+    }
+}
