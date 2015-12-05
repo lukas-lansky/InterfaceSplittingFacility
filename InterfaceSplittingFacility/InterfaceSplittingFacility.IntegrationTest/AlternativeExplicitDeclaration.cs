50096cf6a132d166529f93bbe2e4299b3f19f8f7
Lukáš Lánský <lukas@lansky.name>
2015-12-05 02:37:29 -0800

    List of smaller interfaces should be unbounded; also .NET Framework 4.5 is sufficient

---
+++ InterfaceSplittingFacility/InterfaceSplittingFacility.IntegrationTest/AlternativeExplicitDeclaration.cs
@@ -0,0 +1,98 @@
+﻿#pragma warning disable SA1402
+#pragma warning disable SA1649
+
+namespace InterfaceSplittingFacility.IntegrationTest.AlternativeExplicitDeclaration
+{
+    using Castle.MicroKernel.Registration;
+    using Castle.Windsor;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    public interface IBig
+    {
+        bool SmallProperty1 { get; set; }
+
+        int SmallMethod1();
+
+        int SmallMethod2();
+
+        int SmallMethod3();
+
+        int SmallMethod4();
+    }
+
+    public interface ISmall1
+    {
+        bool SmallProperty1 { get; set; }
+
+        int SmallMethod1();
+    }
+
+    public interface ISmall2
+    {
+        int SmallMethod2();
+
+        int SmallMethod3();
+    }
+
+    public interface ISmall3
+    {
+        int SmallMethod4();
+    }
+
+    [TestClass]
+    public class BasicExplicitDeclaration
+    {
+        [TestMethod]
+        public void AssertIt()
+        {
+            var container = new WindsorContainer().AddFacility<InterfaceSplittingFacility>();
+
+            container.Register(
+                Component.For<ISmall1>().ImplementedBy<DefaultSmall1>().LifestyleTransient(),
+                Component.For<ISmall2>().ImplementedBy<DefaultSmall2>().LifestyleTransient(),
+                Component.For<ISmall3>().ImplementedBy<DefaultSmall3>().LifestyleTransient(),
+                Component.For<IBig>().ImplementedAsSplittedInterfaceBy(typeof(ISmall1), typeof(ISmall2), typeof(ISmall3))
+                );
+
+            var instance = container.Resolve<IBig>();
+            instance.SmallProperty1 = true;
+
+            Assert.AreEqual(instance.SmallProperty1, true);
+            Assert.AreEqual(instance.SmallMethod1(), 1);
+            Assert.AreEqual(instance.SmallMethod2(), 2);
+            Assert.AreEqual(instance.SmallMethod3(), 3);
+            Assert.AreEqual(instance.SmallMethod4(), 4);
+        }
+    }
+
+    public class DefaultSmall1 : ISmall1
+    {
+        public bool SmallProperty1 { get; set; }
+
+        public int SmallMethod1()
+        {
+            return 1;
+        }
+    }
+
+    public class DefaultSmall2 : ISmall2
+    {
+        public int SmallMethod2()
+        {
+            return 2;
+        }
+
+        public int SmallMethod3()
+        {
+            return 3;
+        }
+    }
+
+    public class DefaultSmall3 : ISmall3
+    {
+        public int SmallMethod4()
+        {
+            return 4;
+        }
+    }
+}
--- InterfaceSplittingFacility/InterfaceSplittingFacility/SplittedInterfaceRegistrationExtensions.cs
+++ InterfaceSplittingFacility/InterfaceSplittingFacility/SplittedInterfaceRegistrationExtensions.cs
@@ -2,7 +2,7 @@
 {
     using System.Linq;
     using Castle.MicroKernel.Registration;
-
+    using System;
     public static class SplittedInterfaceRegistrationExtensions
     {
         public static ComponentRegistration<T> ImplementedAsSplittedInterface<T>(this ComponentRegistration<T> componentRegistration)
@@ -10,20 +10,19 @@
         {
             var interfaces = typeof(T).GetInterfaces();
 
-            var openInterceptorType = typeof(InterfaceSplittingInterceptor<>);
-            var closedInterceptorTypes = interfaces.Select(t => openInterceptorType.MakeGenericType(t)).ToArray();
-
-            return componentRegistration.Interceptors(closedInterceptorTypes);
+            return componentRegistration.Interceptors(WrapTypesIntoInterceptor(interfaces));
         }
 
-        public static ComponentRegistration<T> ImplementedAsSplittedInterfaceBy<T, Impl1>(this ComponentRegistration<T> componentRegistration)
+        public static ComponentRegistration<T> ImplementedAsSplittedInterfaceBy<T, Impl1>(
+            this ComponentRegistration<T> componentRegistration)
             where T : class
         {
             return componentRegistration.Interceptors(
                 typeof(InterfaceSplittingInterceptor<Impl1>));
         }
 
-        public static ComponentRegistration<T> ImplementedAsSplittedInterfaceBy<T, Impl1, Impl2>(this ComponentRegistration<T> componentRegistration)
+        public static ComponentRegistration<T> ImplementedAsSplittedInterfaceBy<T, Impl1, Impl2>(
+            this ComponentRegistration<T> componentRegistration)
             where T : class
         {
             return componentRegistration.Interceptors(
@@ -31,13 +30,94 @@
                 typeof(InterfaceSplittingInterceptor<Impl2>));
         }
 
-        public static ComponentRegistration<T> ImplementedAsSplittedInterfaceBy<T, Impl1, Impl2, Impl3>(this ComponentRegistration<T> componentRegistration)
+        public static ComponentRegistration<T> ImplementedAsSplittedInterfaceBy<T, Impl1, Impl2, Impl3>(
+            this ComponentRegistration<T> componentRegistration)
             where T : class
         {
             return componentRegistration.Interceptors(
                 typeof(InterfaceSplittingInterceptor<Impl1>),
                 typeof(InterfaceSplittingInterceptor<Impl2>),
                 typeof(InterfaceSplittingInterceptor<Impl3>));
+        }
+
+        public static ComponentRegistration<T> ImplementedAsSplittedInterfaceBy<T, Impl1, Impl2, Impl3, Impl4>(
+            this ComponentRegistration<T> componentRegistration)
+            where T : class
+        {
+            return componentRegistration.Interceptors(
+                typeof(InterfaceSplittingInterceptor<Impl1>),
+                typeof(InterfaceSplittingInterceptor<Impl2>),
+                typeof(InterfaceSplittingInterceptor<Impl3>),
+                typeof(InterfaceSplittingInterceptor<Impl4>));
+        }
+
+        public static ComponentRegistration<T> ImplementedAsSplittedInterfaceBy<T, Impl1, Impl2, Impl3, Impl4, Impl5>(
+            this ComponentRegistration<T> componentRegistration)
+            where T : class
+        {
+            return componentRegistration.Interceptors(
+                typeof(InterfaceSplittingInterceptor<Impl1>),
+                typeof(InterfaceSplittingInterceptor<Impl2>),
+                typeof(InterfaceSplittingInterceptor<Impl3>),
+                typeof(InterfaceSplittingInterceptor<Impl4>),
+                typeof(InterfaceSplittingInterceptor<Impl5>));
+        }
+
+        public static ComponentRegistration<T> ImplementedAsSplittedInterfaceBy<T, Impl1, Impl2, Impl3, Impl4, Impl5, Impl6>(
+            this ComponentRegistration<T> componentRegistration)
+            where T : class
+        {
+            return componentRegistration.Interceptors(
+                typeof(InterfaceSplittingInterceptor<Impl1>),
+                typeof(InterfaceSplittingInterceptor<Impl2>),
+                typeof(InterfaceSplittingInterceptor<Impl3>),
+                typeof(InterfaceSplittingInterceptor<Impl4>),
+                typeof(InterfaceSplittingInterceptor<Impl5>),
+                typeof(InterfaceSplittingInterceptor<Impl6>));
+        }
+
+        public static ComponentRegistration<T> ImplementedAsSplittedInterfaceBy<T, Impl1, Impl2, Impl3, Impl4, Impl5, Impl6, Impl7>(
+            this ComponentRegistration<T> componentRegistration)
+            where T : class
+        {
+            return componentRegistration.Interceptors(
+                typeof(InterfaceSplittingInterceptor<Impl1>),
+                typeof(InterfaceSplittingInterceptor<Impl2>),
+                typeof(InterfaceSplittingInterceptor<Impl3>),
+                typeof(InterfaceSplittingInterceptor<Impl4>),
+                typeof(InterfaceSplittingInterceptor<Impl5>),
+                typeof(InterfaceSplittingInterceptor<Impl6>),
+                typeof(InterfaceSplittingInterceptor<Impl7>));
+        }
+
+        public static ComponentRegistration<T> ImplementedAsSplittedInterfaceBy<T, Impl1, Impl2, Impl3, Impl4, Impl5, Impl6, Impl7, Impl8>(
+            this ComponentRegistration<T> componentRegistration)
+            where T : class
+        {
+            return componentRegistration.Interceptors(
+                typeof(InterfaceSplittingInterceptor<Impl1>),
+                typeof(InterfaceSplittingInterceptor<Impl2>),
+                typeof(InterfaceSplittingInterceptor<Impl3>),
+                typeof(InterfaceSplittingInterceptor<Impl4>),
+                typeof(InterfaceSplittingInterceptor<Impl5>),
+                typeof(InterfaceSplittingInterceptor<Impl6>),
+                typeof(InterfaceSplittingInterceptor<Impl7>),
+                typeof(InterfaceSplittingInterceptor<Impl8>));
+        }
+
+        public static ComponentRegistration<T> ImplementedAsSplittedInterfaceBy<T>(
+            this ComponentRegistration<T> componentRegistration, params Type[] by)
+            where T : class
+        {
+            return componentRegistration.Interceptors(WrapTypesIntoInterceptor(by));
+        }
+
+        private static Type[] WrapTypesIntoInterceptor(Type[] types)
+        {
+            var openInterceptorType = typeof(InterfaceSplittingInterceptor<>);
+            var closedInterceptorTypes = types.Select(t => openInterceptorType.MakeGenericType(t)).ToArray();
+
+            return closedInterceptorTypes;
         }
     }
 }
